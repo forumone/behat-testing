ccbb7836fd0b1ff9224c3649fa2bd45ca4df9b4f
Daniel McDermott <dmcdermott@forumone.com>
2016-08-03 10:24:25 -0700

    Allow access to nodes, terms and users created via content mocking

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -10,7 +10,11 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Context\Context;
 use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Drupal\DrupalExtension\Hook\Scope\AfterNodeCreateScope;
+use Drupal\DrupalExtension\Hook\Scope\AfterUserCreateScope;
+use Drupal\DrupalExtension\Hook\Scope\AfterTermCreateScope;
 
 /**
  * Defines application features from the specific context.
@@ -22,16 +26,39 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    */
   public function __construct() {
   }
+  public $created_nodes;
+  public $created_users;
+  public $created_terms;
 
   /**
-   * @BeforeSuite
+   * @AfterNodeCreate
    */
-  public static function beforeScenario(BeforeSuiteScope $scope) {
-    $test = 1;
-//    $driver = $this->getMinkParameters();
-//    if ($driver['browser_name'] == 'phantomjs') {
-//      $this->getSession()->getDriver()->resizeWindow(1440, 900);
-//    }
+  public function grabNodeFields(AfterNodeCreateScope $scope) {
+    $this->created_nodes[] = $scope->getEntity();
+  }
+
+  /**
+   * @AfterUserCreate
+   */
+  public function grabUserFields(AfterUserCreateScope $scope) {
+    $this->created_users[] = $scope->getEntity();
+  }
+
+  /**
+   * @AfterTermCreate
+   */
+  public function grabTermFields(AfterTermCreateScope $scope) {
+    $this->created_terms[] = $scope->getEntity();
+  }
+
+  /**
+   * @BeforeScenario
+   */
+  public function alterMinkParameters(BeforeScenarioScope $event) {
+    $driver = $this->getMinkParameters();
+    if ($driver['browser_name'] == 'phantomjs') {
+      $this->getSession()->getDriver()->resizeWindow(1440, 900);
+    }
   }
 
   /**
